7d526fd053d084c779e007764ebc320ff287a827
Dustyroo <dustinkid@hotmail.ca>
2017-11-10 06:05:35 -0700

    Update PubgBot.py

--- PubgBot.py
+++ PubgBot.py
@@ -1,58 +1,138 @@
+import os
 import time
 import random
+import tkinter
 import pyautogui
 
 debug_me = 0
+clear = lambda: os.system('cls')
 
-def fallback_debug_self_fix():
-    if pyautogui.pixelMatchesColor(195, 51, (168, 101, 2)) is True:
-        lobby_check() 
-    elif pyautogui.pixelMatchesColor(185, 719, (20, 20, 20)) is True: 
-        lobby_check()
-    else:
-        pyautogui.alert(text='You broke it', title='Debug could not fix?', button='OK')
+def short_count_down():
+    for i in range(1, 6) [::-1]:
+        print(str(i)+'..')
+        time.sleep(1)
 
-def lobby_check():
+def Stop_Bot():
+    print('Stopped THE BOT!')
+    time.sleep(1000000)
+    Stop_Bot()
+
+
+def lobby_play_check():
     global debug_me
-
+    print('\nDebug level is currently'+'~'+str(debug_me)+'~\n')
     if debug_me == 1:
-        for i in list(range(4)) [::-1]:
-            print(i+i)
-            time.sleep(1)
-        print(pyautogui.position())
-        print(pyautogui.pixel(185, 719)) 
-        print('black')
-        print(pyautogui.pixel(182, 715)) 
-        print('white?')
-        if pyautogui.position() == (0, 0):
-            return
-        debug_me = 0
-        lobby_check()
-
-    if debug_me == 0:
+        time.sleep(2)
+        fallback_debug_self_fix()
+    elif debug_me == 0:
         if pyautogui.pixelMatchesColor(195, 51, (168, 101, 2)) is True:
+            time.sleep(1)
             pyautogui.moveTo(195, 51)
             time.sleep(1)
-            pyautogui.click(77, 715)
+            pyautogui.click(81, 715)
             time.sleep(1)
-            if pyautogui.pixelMatchesColor(185, 719, (20, 20, 20)) is True: 
-                print('done')
-                pyautogui.click(184, 717)
-                print('done')
-            elif pyautogui.pixelMatchesColor(185, 719, (20, 20, 20)) is not True:
-                time.sleep(1)
-                #pyautogui.click(195, 51)
-                print('wow it worked!')
-            else:
-                debug_me = 1
-                lobby_check()
-                
+            start_game_check()
         else:
+            time.sleep(1)
             debug_me = 1
-            print('trying to find pixel')
-            print(debug_me)
-            lobby_check()
+            print('Issue found we are starting to debug!\nThis will take less than a minute!\n')
+            lobby_play_check()
     else:
-        print('Nothing is happening')
-        lobby_check()
-lobby_check()
+        time.sleep(1)
+        print('Nothing is happening\n')
+        fallback_debug_self_fix()
+
+def start_game_check():
+    if pyautogui.pixelMatchesColor(185, 719, (20, 20, 20)) is True:
+        time.sleep(1)
+        pyautogui.click(184, 717)
+        start_game_check()
+    elif pyautogui.pixelMatchesColor(185, 719, (255, 255, 255)) is True:
+        time.sleep(1)
+        #pyautogui.click(195, 51)
+        print('wow it worked!')
+        time.sleep(1)
+        lobby_play_check()
+    else:  
+        time.sleep(1)
+        print('WTF IS GOING ON!')
+        debug_me = 1
+        lobby_play_check()
+
+def long_start_delay():
+    for i in range(1, 10) [::-1]:
+        print(str(i)+'..')
+        time.sleep(1)
+    lobby_play_check()
+    
+def debug_level():
+    print('Debug Starting in:')
+    short_count_down()
+    print('')
+    print(pyautogui.position())
+    print(pyautogui.pixel(185, 719))
+    print('black')
+    print(pyautogui.pixel(182, 715))
+    print('white?')
+    if pyautogui.position() == (0, 0):
+        Stop_Bot()
+    else:
+        debug_me = 0
+        lobby_play_check()
+
+def fallback_debug_self_fix():
+    global debug_me
+    print('')
+    time.sleep(1)
+    for i in range(1, 300):
+        print('Times debug check and tried to fix: '+str(i))
+        if pyautogui.pixelMatchesColor(195, 51, (168, 101, 2)) is True:
+            debug_me = 0
+            print('\nFound and Fixed The Issue!\n')
+            time.sleep(1)
+            lobby_play_check()
+        elif pyautogui.pixelMatchesColor(185, 719, (20, 20, 20)) is True:
+            debug_me = 0
+            print('\nFound and Fixed The Issue!\n')
+            time.sleep(1)
+            start_game_check()
+        elif pyautogui.pixelMatchesColor(185, 719, (255, 255, 255)) is True:
+            debug_me = 0
+            print('\nFound and Fixed The Issue!\n')
+            time.sleep(1)
+            start_game_check()
+        else:
+            print('Cant find anything. Looping over again now!')
+    print('Tried to fix for 20 seconds and could not fix the issue\n'+
+    'Can you please report what happened to Dustyroo? Take screenshots if you can.')
+    Stop_Bot()
+#lobby_play_check()
+
+def test():
+
+    print('Made by: Dustyroo\ndebug current level is '+'~'+str(debug_me)+'~\n\n'+
+    'Screen Resolution Choices are:\n1) 1080p\n2) 1440p\n3) 720p'+
+    '\n4) Other/ None of the above fit my Screen')
+    resolution_choice = input('Please enter your Resolutin Choice: ')
+    if resolution_choice == '1':
+        print('You have chosen: 1) 1080p\nThere is a 10s delay before starting The Bot'
+        + ' \nOpen the game and Wait!')
+        long_start_delay()
+    elif resolution_choice == '2':
+        print('Sorry I have not made a support for this resolution.\n'+
+        'Please contant dustyroo if you want this resolution implemented!')
+        Stop_Bot()
+    elif resolution_choice == '3':
+        print('Sorry I have not made a support for this resolution.\n'+
+        'Please contant dustyroo if you want this resolution implemented!')
+        Stop_Bot()
+    elif resolution_choice == '4':
+        print('If I do not support your resolution contant dustyroo about it in detail and it will be implemented.')
+        Stop_Bot()
+    else:
+        print('you put '+'~'+resolution_choice+'~'+' this is not one of the options\nPlease try again in:')
+        short_count_down()
+        clear()
+        test()
+test()
+
